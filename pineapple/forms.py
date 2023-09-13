ded584c94d10765bc1b94f74a0d749220f662907
AliKazempour <Alikazempoor83@gmail.com>
2023-09-13 23:38:12 +0330

    ✨ feat : add Subscription-form

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -34,8 +34,19 @@ class OrderForm:
             raise forms.ValidationError("۱۰۰ کیلو آناناس میخوای چیکار؟ مشکل داری؟")
         return weight
 
-class SubscriptionForm:
-    pass
+# AK/Subscription-form
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if not phone_number.startswith('09') or len(phone_number) != 11:
+            raise forms.ValidationError(
+                'شماره تلفن اشتباه است. شماره تلفن باید ۱۱ رقم باشد و با ۰۹ شروع شود.')
+
+        return phone_number
 
 class CommentForm:
     pass
