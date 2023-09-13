3fb5673c943e2fd412e2cb1c282277a7e9135792
AliKazempour <Alikazempoor83@gmail.com>
2023-09-13 20:26:44 +0330

    ✨ feat : add Subscription-form

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,4 +1,5 @@
-
+from django import forms
+from .models import Subscription
 
 class SellerForm:
     pass
@@ -9,8 +10,20 @@ class PineappleForm:
 class OrderForm:
     pass
 
-class SubscriptionForm:
-    pass
+# AK/Subscription-form
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if not phone_number.startswith('09') or len(phone_number) != 11:
+            raise forms.ValidationError(
+                'شماره تلفن اشتباه است. شماره تلفن باید ۱۱ رقم باشد و با ۰۹ شروع شود.')
+
+        return phone_number
+
 
 class CommentForm:
     pass
